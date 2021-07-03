0c35fea7a03b65fcbc91e1c987ed77b8b1935936
Tahsina Laboni <74063523+TahsinaSnow@users.noreply.github.com>
2021-07-03 09:55:28 +0600

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,12 @@
+x = input("Enter your marks:")
+x = float(x)
+if x>=80:
+      print("A+")
+elif x>=70:
+    print("A")
+elif x>=60:
+    print("A-")
+elif x>=50:
+    print("B")
+else:
+    print("F")
